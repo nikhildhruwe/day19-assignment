f7ece8bc3dbedbf3e6100b92527efff7225899ca
unknown <nick.dhruwe@gmail.com>
2020-05-31 12:19:07 +0530

    Added Attendace

--- EmpWage.java
+++ EmpWage.java
@@ -3,5 +3,15 @@ class EmpWage {
 	public static void main(String[] args) {
 
 	System.out.println(" Welcome to Employeee Wage Computation ");
+
+	//constants
+	int isFullTime=1;
+		//computations
+	double empCheck = Math.floor(Math.random()*10)%2;
+	if( empCheck == isFullTime)
+		System.out.println("Employee Present");
+	else
+		System.out.println("Employee Absent");
+
 	}
 }
