17cd667d0c979c7a9f8f2146b8be28c607aefb1b
unknown <nick.dhruwe@gmail.com>
2020-05-31 18:21:36 +0530

    added in switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,29 +1,27 @@
 class EmpWage {
 
+	//constants
+	public static final int isFullTime=1;
+	public static final int isPartTime=2;
+	public static final int empRatePerHour=20;
+
+
+
 	public static void main(String[] args) {
 
 	System.out.println(" Welcome to Employeee Wage Computation ");
-
-	//constants
-	int isFullTime=1;
-	int isPartTime=2;
-	int empRatePerHour=20;
 
 	//variables
 	int empHrs=0;
 	int empWage=0;
 	//computation
-	double empCheck = Math.floor(Math.random()*10)%3;
-	if( empCheck == isFullTime){
-		empHrs=8;
+	int empCheck = (int)Math.floor(Math.random()*10)%3;
+	switch ( empCheck){
+					case isFullTime : empHrs=8; break;
+					case isPartTime : empHrs=4; break;
+					default :			empHrs=0; break;
 		}
-	else if (empCheck == isPartTime ){
-		empHrs=4;
-		}
-	else {
-		System.out.println("Employee Absent");
-		empHrs=0;
-		}
+
 		empWage = empHrs*empRatePerHour;
 		System.out.println("Emp Wage = "+empWage);
 	}
